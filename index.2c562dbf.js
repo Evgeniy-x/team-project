0b313dc700609f7117ce5f0de21018999aa8acff
Evgeniy-x <117513013+Evgeniy-x@users.noreply.github.com>
2023-01-22 16:19:48 +0000

    Deploying to gh-pages from @ Evgeniy-x/team-project@a135eef15778fd0c3cd21cfca5ef1be28c083bb4 🚀

--- index.2c562dbf.js
+++ index.2c562dbf.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",(function(){new Swiper(".customers-swiper",{loop:!0,autoHeight:!0,spaceBetween:20,pagination:{el:".swiper-pagination",clickable:!0}})}));
-//# sourceMappingURL=index.d144490e.js.map
+//# sourceMappingURL=index.2c562dbf.js.map
